04e8f33692dee8546bbadf7a63c79b4337f7e6f3
IT23840782 <dinunimweerakoon@gmail.com>
2026-04-11 00:26:41 +0530

    ResourcesCategory created

---
+++ backend/src/main/java/backend/modulea/model/ResourceCategory.java
@@ -0,0 +1,9 @@
+package backend.modulea.model;
+
+public enum ResourceCategory {
+    ACADEMIC,      // Lecture halls, labs, studios, study rooms
+    SPORTS,        // Pools, courts, fields, gyms
+    COMMON,        // Auditoriums, libraries, canteens, open grounds, parking
+    ADMINISTRATIVE, // Offices, meeting rooms, conference halls, reception
+    EQUIPMENT      // Projectors, cameras, laptops, audio systems
+}
