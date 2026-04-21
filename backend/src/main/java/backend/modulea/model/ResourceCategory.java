a29ac047a6a1dd7e4cf1eeb08202d7fafefbad30
IT23840782 <dinunimweerakoon@gmail.com>
2026-04-21 20:39:50 +0530

    change category types

--- backend/src/main/java/backend/modulea/model/ResourceCategory.java
+++ backend/src/main/java/backend/modulea/model/ResourceCategory.java
@@ -1,9 +1,9 @@
 package backend.modulea.model;
 
 public enum ResourceCategory {
-    ACADEMIC,      // Lecture halls, labs, studios, study rooms
-    SPORTS,        // Pools, courts, fields, gyms
-    COMMON,        // Auditoriums, libraries, canteens, open grounds, parking
-    ADMINISTRATIVE, //  meeting rooms, conference halls, auditorium
-    EQUIPMENT      // Projectors, cameras, laptops, audio systems
-}   
+    ACADEMIC,      
+    SPORTS,        
+    COMMON,        
+    ADMINISTRATIVE, 
+    EQUIPMENT      
+    }   
