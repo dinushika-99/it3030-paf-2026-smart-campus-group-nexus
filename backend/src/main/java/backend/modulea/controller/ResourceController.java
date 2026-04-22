1ae4bcd001a3d2779540accb4834db29628f4ccb
Sankalpa-sliit <it23826472@my.sliit.lk>
2026-04-22 15:00:28 +0530

    Nav bar fixed
    Nav bar fixed

--- backend/src/main/java/backend/modulea/controller/ResourceController.java
+++ backend/src/main/java/backend/modulea/controller/ResourceController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController("moduleaResourceController")
 @RequestMapping("/api/resources")
 @CrossOrigin(origins = "*")
 public class ResourceController {
